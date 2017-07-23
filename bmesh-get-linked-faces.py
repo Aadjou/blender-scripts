e50ca12553957fcd0b92a9b98955bccbe15f13c1
Madlaina Kalunder <kalunder@archilogic.com>
2017-07-23 12:26:23 +0200

    Update bmesh-get-linked-faces.py

--- bmesh-get-linked-faces.py
+++ bmesh-get-linked-faces.py
@@ -4,6 +4,7 @@ import bpy
 import bmesh
 
 
+# Simple - get all linked faces
 def get_linked_faces(f):
     
     if f.tag:
@@ -26,6 +27,51 @@ def get_linked_faces(f):
                 # Extend the list with second-degree connected faces
                 f_linked.extend(get_linked_faces(elem))
 
+    return f_linked
+
+
+""" Get linked faces 
+    Args:
+        f ('BMFace') - the current face to check for linked faces
+    Kwargs:
+        stack ('int') - the current recursive stack count.
+        max_angle ('float') - the maximum angle for connected faces in radian
+    Returns: 
+        The connected faces by max angle threshold, material-id, 
+        without exceeding max stack trace
+"""
+def get_linked_faces(f, stack=0, max_angle=3.1416, match_material=False):
+    # Fixme: Find non-recursive alternative 
+    # Make pretty
+    if f.tag:
+        return []
+
+    f_linked = [f]
+    m_idx = f.material_index
+    f.tag = True
+    # Select edges that link two faces
+    edges = [e for e in f.edges if len(e.link_faces) == 2]
+    for e in edges:
+        faces = [elem for elem in e.link_faces if not elem.tag]
+        if not len(faces) == 0:
+            angle = e.calc_face_angle_signed()
+            if angle <= max_angle:
+                if match_material:
+                    for elem in faces:
+                        if f.material_index is m_idx:
+                            # Recursive
+                            if stack < 900:
+                                f_linked.extend(get_linked_faces(elem, stack=stack + 1, max_angle=max_angle, match_material=match_material))
+                            else:
+                                print('Stopped recursive call, else it might exceed maximum stack count.')
+                else:
+                    for elem in faces:
+                        # Recursive
+                        if stack < 900:
+                            f_linked.extend(get_linked_faces(elem, stack=stack + 1, max_angle=max_angle, match_material=match_material))
+                        else:
+                            print('Stopped recursive call, else it might exceed maximum stack count.')
+                            
     return f_linked
 
 def main():
